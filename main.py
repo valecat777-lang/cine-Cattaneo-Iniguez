dc6b9022a7e863de3d75d191866a2a190e96e489
Valentina Cattaneo <valecattaneo1717@gmail.com>
2026-08-03 18:03:35 -0300

    Primer commit del TP, estructura base de las clases

---
+++ main.py
@@ -0,0 +1,45 @@
+'''
+IDEASSSSSSSSSS/cosas que pide el profe:
+
+Login de administradores y clientes (Clase (padre) Usuarios, dsp otra clase Administrador que hereda de usuarios y otra clase Cliente que hereda de usuarios)
+Los administradores administran funciones
+Los clientes compran entradas
+Interfaz gráfica (tkinter otra vez?)
+Código organizado (duh)
+Github y tests D:
+
+'''
+
+class Usuario:
+    def __init__(self, usuario, contraseña):
+        self.usuario = usuario
+        self.contraseña = contraseña
+    def menu(self):
+        pass
+
+class Administrador(Usuario):
+    def __init__(self, usuario, contraseña):
+        super().__init__(usuario, contraseña)
+        # agregar atributos específicos para el administrador o nah? supomgo que si igual :(
+    def menu(self):
+        print("Menu administrador")        
+    #def agregar_funcion(self):
+    #def eliminar_funcion(self):
+    #def modificar_funcion(self):
+
+class Cliente(Usuario):
+    def __init__(self, usuario, contraseña):
+        super().__init__(usuario, contraseña)
+        # nu se, agregar mas atributos especificos del cliente?
+    def menu(self):
+        print("Menu cliente")
+    #def comprar_entrada(self):
+    
+class Funcion:
+    def __init__(self, pelicula, sala, fecha, hora, precio, capacidad):
+        self.pelicula = pelicula
+        self.sala = sala
+        self.fecha = fecha
+        self.hora = hora
+        self.precio = precio
+        self.capacidad = capacidad
